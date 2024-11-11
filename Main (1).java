a3436c5d0bfbe697de72803c6382d84d38699482
PIYUSH SINGH <singhpiyush1403@gmail.com>
2024-11-12 00:41:04 +0530

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,15 @@
+
+
+public class Main{
+    public static void main(String args []){
+        int a = 55, b = 20, c = 30;
+        if(a>b && a>c){
+            System.out.println("a is greater ");}
+            else if (b>c && c>a){
+                System.out.println("b is greater");}
+                else{
+                    System.out.println("c is greater");
+                }
+            }
+                
+}
